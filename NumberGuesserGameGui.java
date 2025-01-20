80731ae6553381bd6285f8d0877b381ee87e6547
Partha-dev01 <parthabrata.ganguly@gmail.com>
2025-01-20 18:38:59 +0530

    Addition of range check

--- NumberGuesserGameGui.java
+++ NumberGuesserGameGui.java
@@ -4,7 +4,11 @@ import java.util.Random;
 
 public class NumberGuesserGameGui extends JFrame {
     private int randomNumber;
-    private int triesLeft = 10;
+    private int triesLeft;
+    private static final int START = 1;
+    private static final int END = 100;
+    private static final int DIFF = 10;
+    private static final int MAX_TRIES = 15;
     private JTextField inputField;
     private JLabel messageLabel;
     private JButton guessButton;
@@ -17,7 +21,7 @@ public class NumberGuesserGameGui extends JFrame {
 
     private void setupUI() {
         setTitle("Number Guesser");
-        setSize(300, 200);
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
@@ -45,26 +49,31 @@ public class NumberGuesserGameGui extends JFrame {
     }
 
     private void setupGame() {
-        randomNumber = new Random().nextInt(100) + 1;
-        messageLabel.setText("Guess a number between 1 and 100!");
-        triesLeft = 10;
+        randomNumber = new Random().nextInt(END - START + 1) + START;
+        messageLabel.setText("Guess a number between " + START + " and " + END + ". Allowed difference: " + DIFF);
+        triesLeft = MAX_TRIES;
         inputField.setEnabled(true);
         guessButton.setEnabled(true);
         inputField.setText("");
+        resetButton.setVisible(false);
     }
 
     private void makeGuess() {
         try {
             int guess = Integer.parseInt(inputField.getText());
             triesLeft--;
+
+            int difference = Math.abs(randomNumber - guess);
             if (guess == randomNumber) {
-                messageLabel.setText("You won! The number was " + randomNumber);
+                messageLabel.setText("Congratulations! You've won! Num was " + randomNumber);
                 endGame();
             } else if (triesLeft == 0) {
                 messageLabel.setText("Game Over! The number was " + randomNumber);
                 endGame();
             } else {
-                messageLabel.setText((guess < randomNumber ? "Try higher! " : "Try lower! ") + "Tries left: " + triesLeft);
+                String rangeMessage = (difference > DIFF ? "Outside range." : "Within range.");
+                String directionMessage = (guess < randomNumber ? " Try higher." : " Try lower.");
+                messageLabel.setText(rangeMessage + directionMessage + " Tries left: " + triesLeft);
             }
         } catch (NumberFormatException e) {
             messageLabel.setText("Please enter a valid number!");
@@ -82,6 +91,7 @@ public class NumberGuesserGameGui extends JFrame {
         dispose();
         new NumberGuesserGameGui().setVisible(true);
     }
+
     public static void main(String[] args) {
         new NumberGuesserGameGui().setVisible(true);
     }
