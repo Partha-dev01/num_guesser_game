8aee78e09c9fe494245db970ded90972c542b1b8
Partha-dev01 <parthabrata.ganguly@gmail.com>
2025-01-21 15:32:09 +0530

    Logarithimic Tries Setting

--- NumberGuesserGameGui.java
+++ NumberGuesserGameGui.java
@@ -8,7 +8,6 @@ public class NumberGuesserGameGui extends JFrame {
     private int start;
     private int end;
     private static final int DIFF = 10;
-    private static final int MAX_TRIES = 15;
     private JTextField inputField;
     private JLabel messageLabel;
     private JButton guessButton;
@@ -21,7 +20,7 @@ public class NumberGuesserGameGui extends JFrame {
 
     private void setupUI() {
         setTitle("Number Guesser");
-        setSize(400, 200);
+        setSize(500, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
@@ -48,25 +47,27 @@ public class NumberGuesserGameGui extends JFrame {
         resetButton.addActionListener(e -> resetGame());
     }
 
+    private int calculateTries(int start, int end) {
+        return (int) (Math.log(end - start + 1) / Math.log(2)) + 2;
+    }
+    
     private void setupGame() {
         Random rand = new Random();
         start = rand.nextInt(1000); 
         end = start + rand.nextInt(5000) + 1000; 
         randomNumber = rand.nextInt(end - start + 1) + start;
-        messageLabel.setText("Guess a number between " + start + " and " + end + ". Allowed difference: " + DIFF);
-        triesLeft = MAX_TRIES;
+        triesLeft = calculateTries(start, end);
+        messageLabel.setText("Guess a number between " + start + " and " + end + ". Allowed difference: " + DIFF + ". Tries: " + triesLeft);
         inputField.setEnabled(true);
         guessButton.setEnabled(true);
         inputField.setText("");
         resetButton.setVisible(false);
     }
-    
 
     private void makeGuess() {
         try {
             int guess = Integer.parseInt(inputField.getText());
             triesLeft--;
-
             int difference = Math.abs(randomNumber - guess);
             if (guess == randomNumber) {
                 messageLabel.setText("Congratulations! You've won! Num was " + randomNumber);
